da91e23b2858e2506d2681b6ee6f5cad82dec938
ThePegaz <kasperi.kittila@gmail.com>
2016-01-16 20:06:07 +0200

    Array + Async functions done

--- app/arrays.js
+++ app/arrays.js
@@ -31,8 +31,8 @@ exports.arraysAnswers = {
   removeWithoutCopy: function (arr, item) {
     var i, len;
 
-    for(i = 0, len = arr.length; i < len; i++) {
-      if(arr[i] === item) {
+    for (i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
         arr.splice(i, 1);
         i = i - 1;
         len = len - 1;
@@ -42,42 +42,80 @@ exports.arraysAnswers = {
   },
 
   append: function (arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function (arr) {
-
+    arr.splice(-1, 1);
+    return arr;
   },
 
   prepend: function (arr, item) {
-
+    var newArr = [];
+    newArr.push(item);
+    for (var i = 0; i < arr.length; i++) {
+      newArr.push(arr);
+    }
+    return newArr;
   },
 
   curtail: function (arr) {
-
+    arr.splice(0, 1);
+    return arr;
   },
 
   concat: function (arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function (arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function (arr, item) {
-
+    var sum = 0;
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        sum += 1;
+      }
+    }
+    return sum;
   },
 
   duplicates: function (arr) {
+    arr.sort();
+    var i,
+      len,
+      duplicatesArray = [],
+      obj = {};
 
+    for (i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === arr[i + 1]) {
+        obj[arr[i]] = 0;
+      }
+    }
+    for (i in obj) {
+      duplicatesArray.push(Number(i));
+    }
+    return duplicatesArray;
   },
 
   square: function (arr) {
-
+    for (var i = 0, len = arr.length; i < len; i++) {
+      arr[i] = arr[i] * arr[i];
+    }
+    return arr;
   },
 
   findAllOccurrences: function (arr, target) {
-
+    var newArr = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if(arr[i] === target) {
+        newArr.push(i);
+      }
+    }
+    return newArr;
   }
 };
--- app/async.js
+++ app/async.js
@@ -1,11 +1,22 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
-
+  async: function (value) {
+    var dfd = $.Deferred();
+    setTimeout(function () {
+      dfd.resolve(value);
+    }, 10);
+    return dfd.promise();
   },
 
-  manipulateRemoteData : function(url) {
-
+  manipulateRemoteData: function (url) {
+    var dfd = $.Deferred();
+    $.getJSON(url).then(function (result) {
+      var arr = $.map(result.people, function (person) {
+        return person.name;
+      });
+      dfd.resolve(arr.sort());
+    });
+    return dfd.promise();
   }
 };
